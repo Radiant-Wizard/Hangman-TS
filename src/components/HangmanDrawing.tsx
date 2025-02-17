cc4555c02e988fbf90a195676e75d4ce2762ee54
radiant_wizard <hei.taratra@gmail.com>
2025-02-17 21:25:14 +0300

    feat: created the drawing of the hanging bar

---
+++ src/components/HangmanDrawing.tsx
@@ -0,0 +1,12 @@
+import '../style/HangmanDrawing.css';
+
+export function HangmanDrawing(){
+    return (
+        <div className="hang-component">
+            <div className="hang-cord"/>
+            <div className="hang-top"/>
+            <div className="hang-bar"/>
+            <div className="hang-base"/>
+        </div>
+    )
+}
